25ec1f2d7c88b646c1921539539a9f63e5a5ef8f
Vilberg <vilbergs@gmail.com>
2024-06-23 22:33:02 +0200

    WIP Update screen with value

--- uppgift_2/main/main.cpp
+++ uppgift_2/main/main.cpp
@@ -4,6 +4,11 @@
 #include <LCD.h>
 #include <I2CBus.h>
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "mpu6050.h"
+#include <string>
+#include <format>
 
 static const char *TAG = "MAIN";
 
@@ -43,6 +48,7 @@ static esp_err_t i2c_master_init(void)
 }
 
 LCD *lcd;
+mpu6050_handle_t mpu6050;
 
 extern "C" void app_main(void)
 {
@@ -50,18 +56,31 @@ extern "C" void app_main(void)
     ESP_LOGI(TAG, "I2C initialized successfully for master");
 
     lcd = new LCD();
-
     lcd->initialize();
 
-    for (int i = 0; i < 16; i++)
+    mpu6050 = mpu6050_create(I2C_MASTER_NUM, MPU6050_I2C_ADDRESS);
+    mpu6050_wake_up(mpu6050); // Make sure the device is awake
+    mpu6050_config(mpu6050, mpu6050_acce_fs_t::ACCE_FS_8G, mpu6050_gyro_fs_t::GYRO_FS_500DPS);
+
+    mpu6050_acce_value_t acce;
+    mpu6050_gyro_value_t gyro;
+
+    for (;;)
     {
-        lcd->display_text("Hello, World!");
-        vTaskDelay(pdMS_TO_TICKS(60));
+
+        mpu6050_get_acce(mpu6050, &acce);
+        mpu6050_get_gyro(mpu6050, &gyro);
+
+        // ESP_LOGI(TAG, "Acce: %f %f %f", acce.acce_x, acce.acce_y, acce.acce_z);
+        // ESP_LOGI(TAG, "Gyro: %f %f %f", gyro.gyro_x, gyro.gyro_y, gyro.gyro_z);
+
+        // lcd->display_text(std::format("Acce: {} {} {}", acce.acce_x, acce.acce_y, acce.acce_z));
+        lcd->display_text(std::format("Gyro: {}", gyro.gyro_x));
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
 
         lcd->clear_display();
-        vTaskDelay(pdMS_TO_TICKS(15));
-
-        vTaskDelay(pdMS_TO_TICKS(60));
+        vTaskDelay(pdMS_TO_TICKS(50));
     }
 
     ESP_LOGI(TAG, "Gracefully de-initializing I2C");
@@ -71,4 +90,7 @@ extern "C" void app_main(void)
 
     ESP_ERROR_CHECK(i2c_driver_delete(I2C_MASTER_NUM));
     ESP_LOGI(TAG, "I2C de-initialized successfully");
+
+    delete lcd;
+    mpu6050_delete(mpu6050);
 }
